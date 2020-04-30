38590393591c74485a61c9100dc9b17b8efd5757
vinmugambi <vinmugambi17@gmail.com>
2020-04-30 20:44:07 +0300

    initial commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="bg-grey-lighter py-8 mt-4 text-center">
+    <div class="container">
+        <p>Footer</p>
+        @php dynamic_sidebar('sidebar-footer') @endphp
+    </div>
+</footer>
