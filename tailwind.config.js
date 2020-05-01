e86aa0b3e076a91a8264882cdaa3bf73cd2afcda
vinmugambi <vinmugambi17@gmail.com>
2020-05-01 19:28:42 +0300

    now understand glob syntax for matching files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,7 @@ module.exports = {
     purge: {
         enabled: true,
         content: [
-            "./resources/views/*.blade.php",
-            "./resources/views/layouts/*.blade.php",
-            "./resources/views/partials/*.blade.php"
+            "./resources/views/**/*.blade.php"
         ]
     },
     theme: {
