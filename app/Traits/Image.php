ccbf334514aa1f40ccf60a2e1172e2e3a587a18a
vinmugambi <vinmugambi17@gmail.com>
2020-05-04 15:15:02 +0300

    Reusable image class

---
+++ app/Traits/Image.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Traits;
+
+trait Image
+{
+    protected function getImage($id, $size = "medium")
+    {
+        $project_image = get_field("project_image", $id);
+
+        return (object) [
+            "alt" => $project_image["alt"],
+            "caption" => $project_image["title"],
+            "src" => $project_image["sizes"][$size],
+            "width" => $project_image["sizes"][$size . "-width"],
+            "height" => $project_image["sizes"][$size . "-height"],
+        ];
+    }
+}
