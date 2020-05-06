61d7324b2d7c4adc9a387b85892b182d019efaa8
vinmugambi <vinmugambi17@gmail.com>
2020-05-06 22:55:34 +0300

    add support for new tailwind lengths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
     purge: ["./resources/views/**/*.blade.php"],
     theme: {
-        extend: {}
+        extend: {
+            spacing: {
+                "72": "18rem",
+                "84": "21rem",
+                "96": "24rem"
+            }
+        }
     },
     variants: {},
     plugins: []
