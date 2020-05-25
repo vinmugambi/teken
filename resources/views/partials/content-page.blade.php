8d14fc80a451c2e74534a154ba95f980f2c03cdc
vinmugambi <vinmugambi17@gmail.com>
2020-05-25 16:35:43 +0300

    add contact page

--- resources/views/partials/content-page.blade.php
+++ resources/views/partials/content-page.blade.php
@@ -1,5 +1,5 @@
-<section id="content" class="container mx-auto h-full flex items-center">
-    <div class="max-w-4xl w-full mx-auto text-lg px-2 sm:px-4 pt-12 pb-24">
+<section id="content" class="container mx-auto flex items-center">
+    <div class="max-w-4xl w-full mx-auto text-lg px-2 sm:px-4 pt-8 pb-16">
         <div class="max-w-2xl wp-markdown">
             {!!$post->post_content!!}
         </div>
--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,5 +1,5 @@
 <section id="header" class="h-40 bg-cover"
-    style="background-image: linear-gradient( hsla(240,90%,10%,0.8), hsla(240,100%,70%,0.5)), url({{App::glideImage()}});">
+    style="background-image: linear-gradient(to right, hsla(240,90%,10%,0.8), hsla(240,100%,40%,0.5)), url({{App::glideImage()}});">
     <div class="container mx-auto h-full flex items-center">
         <div class="max-w-4xl w-full mx-auto text-xl text-white px-2 sm:px-4">
                 <h1 class="text-6xl">
